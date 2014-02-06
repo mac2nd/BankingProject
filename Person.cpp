349acb3f5af325d345ec852ac343095b2dfa2b28
mac2nd <cantom12@highpoint.edu>
2014-02-06 15:54:15 -0500

    Update Person.cpp

--- Person.cpp
+++ Person.cpp
@@ -18,4 +18,92 @@ Person::~Person() {
 	// TODO Auto-generated destructor stub
 }
 
+void Person::setName(string n)
+{
+	name = n;
+}
+
+string Person::getName()
+{
+	return  name;
+}
+
+void Person::setBirthday(string b)
+{
+	birthday = b;
+}
+
+string Person::getBirthday()
+{
+	return birthday;
+}
+
+void Person::setGender(char g)
+{
+	gender = g;
+}
+
+string Person::getGender()
+{
+	return gender;
+}
+
+void Person::setPhoneNumber(string p)
+{
+	phoneNumber = p;
+}
+
+string Person::getPhoneNumber()
+{
+	return  phoneNumber;
+}
+
+void Person::setAddress(string a)
+{
+	address = a;
+}
+
+string Person::getAddress()
+{
+	return address;
+}
+
+void Person::setEmail(string e)
+{
+	email = e;
+}
+
+string Person::getEmail()
+{
+	return email;
+}
+
+string Person::getUserName()
+{
+	return userName;
+}
+
+void Person::setUserName(string un)
+{
+	userName = un;
+}
+
+void Person::setPerson(string n, string b, char g, string p, string a, string e, string un)
+{
+
+}
+
+void Person::displayPersonalInformation()
+{
+	/*string n;
+	n=Person.getName();
+	cout<<n;
+	*/
+}
+
+void Person::printOptions()
+{
+
+}
+}
 }
